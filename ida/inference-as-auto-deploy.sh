2788801aa6c207531e51fbf0edf712f61b1690fb
AhmedSeemalK <ahmed.seemal@intel.com>
2025-04-15 16:02:15 +0530

    Update inference-as-auto-deploy.sh

--- ida/inference-as-auto-deploy.sh
+++ ida/inference-as-auto-deploy.sh
@@ -467,7 +467,13 @@ deploy_observability_playbook() {
 }
 
 deploy_cluster_config_playbook() {    
-    ansible-playbook -i "${INVENTORY_PATH}" playbooks/deploy-cluster-config.yml --become --become-user=root --extra-vars "secret_name=${cluster_url} cert_file=${cert_file} key_file=${key_file}"    
+    if [ "${deploy_observability}" = "on" ]; then
+        tags="deploy_admin_dashboard"
+    else
+        tags=""        
+    fi
+    
+    ansible-playbook -i "${INVENTORY_PATH}" playbooks/deploy-cluster-config.yml --become --become-user=root --extra-vars "secret_name=${cluster_url} cert_file=${cert_file} key_file=${key_file}" --tags "$tags"
 }
 
 
